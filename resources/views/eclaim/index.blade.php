a6b061f088fd9a7220ccdb8ce2114d8e589207d1
rq-admin <111732382+rq-admin@users.noreply.github.com>
2024-02-27 14:37:54 +0000

    add edit action

--- resources/views/eclaim/index.blade.php
+++ resources/views/eclaim/index.blade.php
@@ -42,26 +42,40 @@
                                     <td>{{ $eclaim->description }}</td>
                                     <td class="Action">
                                         <span>
-                                            @can('Edit Eclaim')
-                                                <div class="action-btn bg-info ms-2">
-                                                    <a href="#" class="mx-3 btn btn-sm  align-items-center"
-                                                        data-url="{{ URL::to('eclaim/' . $eclaim->id . '/edit') }}"
-                                                        data-ajax-popup="true" data-size="md" data-bs-toggle="tooltip" title=""
-                                                        data-title="{{ __('Edit Eclaim') }}"
-                                                        data-bs-original-title="{{ __('Edit') }}">
-                                                        <i class="ti ti-pencil text-white"></i>
-                                                    </a>
-                                                </div>
-                                            @endcan
+                                            @if (\Auth::user()->type == 'employee' && $eclaim->status=="pending")
+                                                @can('Edit Eclaim')
+                                                    <div class="action-btn bg-info ms-2">
+                                                        <a href="#" class="mx-3 btn btn-sm  align-items-center"
+                                                            data-url="{{ URL::to('eclaim/' . $eclaim->id . '/edit') }}"
+                                                            data-ajax-popup="true" data-size="md" data-bs-toggle="tooltip" title=""
+                                                            data-title="{{ __('Edit Eclaim') }}"
+                                                            data-bs-original-title="{{ __('Edit') }}">
+                                                            <i class="ti ti-pencil text-white"></i>
+                                                        </a>
+                                                    </div>
+                                                @endcan
 
-                                            @can('Delete Eclaim')
-                                                <div class="action-btn bg-danger ms-2">
-                                                    {!! Form::open(['method' => 'DELETE', 'route' => ['document.destroy', $eclaim->id], 'id' => 'delete-form-' . $eclaim->id]) !!}
-                                                    <a href="#" class="mx-3 btn btn-sm  align-items-center bs-pass-para"
-                                                        data-bs-toggle="tooltip" title="" data-bs-original-title="Delete"
-                                                        aria-label="Delete"><i
-                                                            class="ti ti-trash text-white text-white"></i></a>
-                                                    </form>
+                                                @can('Delete Eclaim')
+                                                    <div class="action-btn bg-danger ms-2">
+                                                        {!! Form::open(['method' => 'DELETE', 'route' => ['eclaim.destroy', $eclaim->id], 'id' => 'delete-form-' . $eclaim->id]) !!}
+                                                        <a href="#" class="mx-3 btn btn-sm  align-items-center bs-pass-para"
+                                                            data-bs-toggle="tooltip" title="" data-bs-original-title="Delete"
+                                                            aria-label="Delete"><i
+                                                                class="ti ti-trash text-white text-white"></i></a>
+                                                        </form>
+                                                    </div>
+                                                @endcan
+                                            @endif
+
+                                            @can('Manage Eclaim')
+                                                <div class="action-btn bg-info ms-2">
+                                                    <a href="#" class="mx-3 btn btn-sm bg-info  align-items-center"
+                                                        data-url="{{ URL::to('eclaim/' . $eclaim->id . '/receipt') }}"
+                                                        data-ajax-popup="true" data-size="md" data-bs-toggle="tooltip" title=""
+                                                        data-title="{{ __('Eclaim Receipt') }}"
+                                                        data-bs-original-title="{{ __('View Receipt') }}">
+                                                        <i class="ti ti-eye text-white"></i>
+                                                    </a>
                                                 </div>
                                             @endcan
                                         </span>
