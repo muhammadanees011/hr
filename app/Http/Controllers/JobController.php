b112f8b5ce5935807c12da73b7a0a21c27f2955d
rq-admin <111732382+rq-admin@users.noreply.github.com>
2024-03-08 17:42:58 +0000

    Revert "contracts"
    This reverts commit 36ae8f05eedc74ae9a705b8cf858d3527e57cab1.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -9,7 +9,6 @@ use App\Models\JobApplication;
 use App\Models\JobApplicationNote;
 use App\Models\JobAttachment;
 use App\Models\JobCategory;
-use App\Models\Department;
 use App\Models\User;
 use App\Models\JobStage;
 use App\Models\JobWordCount;
@@ -58,13 +57,11 @@ class JobController extends Controller
         $branches = Branch::where('created_by', \Auth::user()->creatorId())->get()->pluck('name', 'id');
         $branches->prepend('All', 0);
 
-        $departments = Department::all()->pluck('name', 'id');
-
         $status = Job::$status;
 
         $customQuestion = CustomQuestion::where('created_by', \Auth::user()->creatorId())->get();
 
-        return view('job.create', compact('categories', 'departments', 'status', 'branches', 'customQuestion'));
+        return view('job.create', compact('categories', 'status', 'branches', 'customQuestion'));
     }
 
 
@@ -78,7 +75,7 @@ class JobController extends Controller
                 [
                     'title' => 'required',
                     'branch' => 'required',
-                    'department_id' => 'required',
+                    'department' => 'required',
                     'contract_type' => 'required',
                     'category' => 'required',
                     'skill' => 'required',
@@ -99,7 +96,7 @@ class JobController extends Controller
             $job                  = new Job();
             $job->title           = $request->title;
             $job->branch          = $request->branch;
-            $job->department      = $request->department_id;
+            $job->department      = $request->department;
             $job->contract_type   = $request->contract_type;
             $job->category        = $request->category;
             $job->skill           = $request->skill;
@@ -164,7 +161,7 @@ class JobController extends Controller
                 [
                     'title' => 'required',
                     'branch' => 'required',
-                    'department_id' => 'required',
+                    'department' => 'required',
                     'contract_type' => 'required',
                     'category' => 'required',
                     'skill' => 'required',
